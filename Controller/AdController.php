955ea3664ac8debfe2a97228fb78f7d553463eb0
lsroudi <lsroudi@gmail.com>
2014-06-23 18:21:50 +0200

    add list action to generate list of ads

--- Controller/AdController.php
+++ Controller/AdController.php
@@ -18,6 +18,10 @@ use Lsroudi\ClassifiedAdsBundle\LsroudiClassifiedAdsEvents;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Lsroudi\ClassifiedAdsBundle\Event\FilterAdResponseEvent;
 use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
+use Lsroudi\ClassifiedAdsBundle\Event\FilterQueryForAdEvent;
+use Lsroudi\ClassifiedAdsBundle\Event\FilterListAdResponseEvent;
+use Pagerfanta\Adapter\DoctrineORMAdapter;
+use Pagerfanta\Pagerfanta;
 
 
 /**
@@ -98,6 +102,54 @@ class AdController
             'form' => $form->createView()
         ));
     }
+    
+    
+    /**
+     * @Route("/list/{page}", defaults={"page" = 1},name="ad_list")
+     */
+    public function listAction($page)
+    {
+        /** @var $adManager \Lsroudi\ClassifiedAdsBundle\Manager\AdManager */
+        $adManager = $this->container->get('lsroudi_classified_ads.ad.manager');
+        /** @var $dispatcher \Symfony\Component\EventDispatcher\EventDispatcherInterface */
+        $dispatcher = $this->container->get('event_dispatcher');          
+
+        $qb = $adManager->findAds();
+//        die(var_dump($qb));
+        /* customize query with event pattern */
+        $request = $this->container->get('request');
+        
+        $queryevent = new FilterQueryForAdEvent($request);
+        $dispatcher->dispatch(LsroudiClassifiedAdsEvents::AD_LIST_INIT, $queryevent);
+        
+        if (null != $queryevent->getQuery()){  
+            $qb = $queryevent->getQuery();
+        }        
+        
+        $adapter = new DoctrineORMAdapter($qb);
+        $pagerfanta = new Pagerfanta($adapter);
+        try {
+            $entities = $pagerfanta
+                    ->setMaxPerPage(5)
+                    ->setCurrentPage($page)
+                    ->getCurrentPageResults();
+        } catch (\Pagerfanta\Exception\NotValidCurrentPageException $e) {
+            throw new \Exception("Page not found.");
+        }
+        
+        $event = new FilterListAdResponseEvent($entities, $pagerfanta);
+        $dispatcher->dispatch(LsroudiClassifiedAdsEvents::AD_LIST_GENERATED, $event);
+
+        if (null != $response = $event->getResponse()){
+            
+            return $response;
+        }
+        
+        return $this->templating->renderResponse('LsroudiClassifiedAdsBundle:Ad:list.html.twig', array(
+                    'entities' => $entities,
+                    'pager' => $pagerfanta
+        ));        
+    }    
     
     /**
      * @Route("/confirmed", name="lsroudi_classified_ads.ad_add_confirmed")
